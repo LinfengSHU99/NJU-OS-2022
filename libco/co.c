ed11e36030147ac888b83d8982022ea83abf062e
tracer-nju <tracer@nju.edu.cn>
2022-04-29 00:10:37 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:10:37 up 10:12,  1 user,  load average: 0.54, 0.50, 0.45

--- libco/co.c
+++ libco/co.c
@@ -48,14 +48,14 @@ __attribute__((constructor)) void init() {
   srand(666);
 }
 
-static inline void stack_switch_call(void *sp, void *entry, void* arg) {
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t * arg) {
     asm volatile (
 #if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
     : : "b"((uintptr_t *)sp), "d"(entry), "a"(arg) : "memory"
 #else
     "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
-      : : "b"((u_int*)sp - 8), "d"(entry), "a"(arg) : "memory"
+      : : "b"((uintptr_t*)sp - 8), "d"(entry), "a"(arg) : "memory"
 #endif
     );
 }
