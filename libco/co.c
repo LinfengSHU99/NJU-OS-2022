ed23f99a127c788144ee3c03aaef63d49b973205
tracer-nju <tracer@nju.edu.cn>
2022-04-28 18:05:14 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:05:14 up  4:07,  1 user,  load average: 0.32, 0.35, 0.32

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,109 @@
 #include "co.h"
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <setjmp.h>
+// #include <unistd.h>
 
-struct co {
-};
+#define MAX_NUM 1000
+#define NOT_RUNNING 0
+#define RUNNING 1
+
+extern int main();
+
+typedef struct co {
+  void*(*entry)(void*);
+  int id;
+  char *name;
+  void *arg;
+  jmp_buf buf;
+  int mode;
+}co;
+
+typedef struct Node {
+  co* co;
+  Node* next;
+}Node;
+
+Node* head;
+Node* tail;
+co *co_main;
+co *cur_co;
+
+__attribute__((constructor)) void init() {
+  head = (Node*)malloc(sizeof(Node));
+  head->co = NULL;
+  head->next = NULL;
+  tail = head;
+  co_main = co_start("main", main, NULL);
+  cur_co = co_main;
+  co_main->mode = RUNNING;
+  srand(1);
+}
+
+void remove(int id) {
+  Node* p = head;
+  while (p->next != NULL) {
+    if (p->next->co->id == id) {
+      free(p->next->co);
+      Node* t = p->next;
+      p->next = p->next->next;
+      free(t);
+    }
+    p = p->next;
+  }
+  size--;
+}
+
+int cur_num = 0;
+int size = 0;
+
+co *random_chose() {
+  int index = rand() % size + 1;
+  Node* p = head;
+  while (index--) {
+    p = p->next;
+  }
+  return p->co;
+}
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+  assert(cur_num < MAX_NUM);
+  co* co_ptr = (co*)malloc(sizeof(co));
+  co_ptr->entry = func;
+  co_ptr->id = cur_num;
+  strcpy(co_ptr->name, name);
+  co_ptr->arg = arg;
+  co_ptr->mode = NOT_RUNNING;
+  Node* node = (Node*)malloc(sizeof(Node));
+  node->co = co_ptr;
+  node->next = NULL;
+  tail->next = node;
+  tail = node;
+  cur_num++;
+  size++;
+  return co_ptr;
 }
 
 void co_wait(struct co *co) {
+  
+  cur_co = co;
+
+  co->mode = RUNNING;
+  if (co != co_main){
+    co->entry(co->arg);
+    remove(co->id);
+  }
+  else {
+    return;
+  }
 }
 
 void co_yield() {
+  setjmp(cur_co->buf);
+  co *next_co = random_chose();
+  if (next_co->mode == RUNNING) longjmp(next_co->buf);
+  else if(next_co->mode == NOT_RUNNING) {
+    co_wait(next_co);
+  }
 }
