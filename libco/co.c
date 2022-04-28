450af037e391faf0fc2ed50095fa01c658f834bd
tracer-nju <tracer@nju.edu.cn>
2022-04-28 21:31:28 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:31:28 up  7:33,  1 user,  load average: 0.60, 0.41, 0.30

--- libco/co.c
+++ libco/co.c
@@ -40,7 +40,7 @@ __attribute__((constructor)) void init() {
   co_main = co_start("main", (void*)main, NULL);
   cur_co = co_main;
   co_main->mode = RUNNING;
-  srand(1);
+  srand(2);
 }
 
 void remove(int id) {
