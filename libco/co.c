aae6108c29011387bb6dab377de50916a9d6750e
tracer-nju <tracer@nju.edu.cn>
2022-04-30 21:45:57 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:57 up  1:36,  1 user,  load average: 0.54, 0.54, 0.44

--- libco/co.c
+++ libco/co.c
@@ -153,7 +153,7 @@ void co_wait(struct co *co) {
           stack_switch_call(sp, co->entry, (uintptr_t )co->arg);
           cur_co->entry(cur_co->arg);
 //          set_rsp(sp_stack[--top]);
-          longjmp(buf_stack[top--], 1);
+          longjmp(buf_stack[--top], 1);
       }
       else {
           remove_co(co->id);
