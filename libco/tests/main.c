94c06cc0ad3a418b6a849c7c302fdf5ecc72368d
tracer-nju <tracer@nju.edu.cn>
2022-05-01 14:05:27 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:05:27 up  1:35,  1 user,  load average: 0.06, 0.23, 0.21

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -17,7 +17,7 @@ static int get_count() {
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     for (int i = 0; i < 100; ++i) {
-        printf("%s%d  \n", s, get_count());
+        printf("%s%d  ", s, get_count());
         add_count();
         co_yield();
     }
@@ -58,7 +58,7 @@ static void do_produce(Queue *queue) {
         return;
     }
     memset(item->data, 0, 10);
-    sprintf(item->data, "libco-%d", g_count++);
+    sprintf(item->data, "libco-%d\n", g_count++);
     q_push(queue, item);
 }
 
