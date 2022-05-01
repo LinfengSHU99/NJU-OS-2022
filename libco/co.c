8837de3f828f824b9752bae0fead1b2dea0d1c0b
tracer-nju <tracer@nju.edu.cn>
2022-05-01 15:06:45 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:06:45 up  2:36,  1 user,  load average: 0.34, 0.21, 0.19

--- libco/co.c
+++ libco/co.c
@@ -5,8 +5,6 @@
 #include <assert.h>
 #include <setjmp.h>
 #include <stdint.h>
-//#include <stddef.h>
-// #include <unistd.h>
 
 #define MAX_NUM 1000
 #define NOT_RUNNING 0
@@ -26,7 +24,7 @@ typedef struct co {
   int mode;
   uint8_t stack[SIZE];
   uintptr_t waiter_sp;
-  struct co* waiter;
+//  struct co* waiter;
 }co;
 
 typedef struct Node {
@@ -40,11 +38,10 @@ co *co_main;
 co *cur_co;
 int cur_num = 0;
 int size = 0;
-//jmp_buf buf_stack[STACK_SIZE];
 uintptr_t sp_stack[SIZE];
 int top = 0;
-//uintptr_t main_sp;
 int cnt = 1;
+
 __attribute__((constructor)) void init() {
   head = (Node*)malloc(sizeof(Node));
   head->co = NULL;
@@ -74,7 +71,6 @@ void remove_co(int id) {
   Node* p = head;
   while (p->next != NULL) {
     if (p->next->co->id == id) {
-//        free(p->next->co->stack);
       free(p->next->co);
       Node* t = p->next;
       p->next = p->next->next;
@@ -105,12 +101,10 @@ co *random_chose() {
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   assert(cur_num < MAX_NUM);
   co* co_ptr = (co*)malloc(sizeof(co));
-//  co_ptr->stack = (uint8_t*) malloc(sizeof(uint8_t) * STACK_SIZE);
   memset(co_ptr->stack, 0, SIZE);
   co_ptr->entry = func;
   co_ptr->id = cur_num;
   co_ptr->waiter_sp = 0;
-  co_ptr->waiter = NULL;
   strcpy(co_ptr->name, name);
   co_ptr->arg = arg;
   co_ptr->mode = NOT_RUNNING;
@@ -153,7 +147,6 @@ void co_wait(struct co *co) {
       return;
   }
   cur_co = co;
-
   if (co->mode == RUNNING) {
       longjmp(co->buf, 1);
   }
@@ -161,35 +154,17 @@ void co_wait(struct co *co) {
       co->mode = RUNNING;
       if (co != co_main){
           void *sp = get_sp(co);
-//          sp_stack[top++] = get_rsp();
         co->waiter_sp = get_rsp();
-//      int r = setjmp(buf_stack[top++]);
-//      if (r == 0) {
           stack_switch_call(sp, co->entry, (uintptr_t )co->arg);
-//      set_rsp(sp_stack[--top]);
           co->entry(cur_co->arg);
-//          set_rsp(sp_stack[--top]);
             co->mode = DEAD;
           set_rsp((uintptr_t)co->waiter_sp);
-//          longjmp(buf_stack[--top], 1);
-//      }
-//      else {
-//          remove_co(co->id);
-//      }
-//      stack_switch_back();
-//    co->entry(co->arg);
-
       }
-//      else {
-//          return;
-//      }
   }
     remove_co(co->id);
-
 }
 
 void co_yield() {
-//    stack_switch_call(co->stack, co->entry, (uintptr_t )co->arg);
   if (cur_co == co_main) return;
   int r = setjmp(cur_co->buf);
   if (r == 0) {
@@ -200,7 +175,6 @@ void co_yield() {
         longjmp(next_co->buf, 1);
     }
     else if(next_co->mode == NOT_RUNNING) {
-//      co_wait(next_co);
         cur_co = next_co;
         next_co->mode = RUNNING;
         void *sp = get_sp(next_co);
@@ -212,5 +186,4 @@ void co_yield() {
     }
   }
   else if (r == 1) return;
-  
 }
