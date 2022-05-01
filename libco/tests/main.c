10422e3e7a2d59644a73444b104364e871d18ed1
tracer-nju <tracer@nju.edu.cn>
2022-05-01 14:20:35 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:20:35 up  1:50,  1 user,  load average: 0.64, 0.35, 0.24

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -64,7 +64,7 @@ static void do_produce(Queue *queue) {
 
 static void producer(void *arg) {
     Queue *queue = (Queue*)arg;
-    for (int i = 0; i < 4; ) {
+    for (int i = 0; i < 2; ) {
         if (!q_is_full(queue)) {
             // co_yield();
             do_produce(queue);
