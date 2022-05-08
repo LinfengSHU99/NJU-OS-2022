4a80d5386ea8af583dd4137b0cef98a37616c58c
tracer-nju <tracer@nju.edu.cn>
2022-05-09 00:36:03 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:36:03 up  2:34,  1 user,  load average: 0.29, 0.42, 0.35

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,6 +1,8 @@
 #include <kernel.h>
 // #include <klib.h>
 // #include <klib-macros.h>
-// #include <stddef.h>
-// #include <stdlib.h>
-// #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define HEAP_SIZE 134217728
