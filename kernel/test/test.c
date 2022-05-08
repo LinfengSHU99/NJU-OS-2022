bc65d9aa000db789e68884d4d9fb7f346140ee7d
tracer-nju <tracer@nju.edu.cn>
2022-05-09 00:22:59 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:22:59 up  2:21,  1 user,  load average: 0.44, 0.37, 0.23

---
+++ kernel/test/test.c
@@ -0,0 +1,12 @@
+#include "thread.h"
+
+// extern 
+static void entry(int tid) { pmm->alloc(128); }
+static void goodbye()      { printf("End.\n"); }
+
+int main() {
+  pmm->init();
+  for (int i = 0; i < 4; i++)
+    create(entry);
+  join(goodbye);
+}
