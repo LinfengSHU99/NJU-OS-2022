a4172d24c0450316488121398b6fc8a80adcb547
tracer-nju <tracer@nju.edu.cn>
2022-05-11 15:20:22 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:20:22 up  2:12,  1 user,  load average: 0.21, 0.15, 0.12

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -4,12 +4,87 @@
 #include <common.h>
 #endif
 
+#define NUM 1000
+int lock = 0;
+
+typedef struct Node {
+  uintptr_t start;
+  uintptr_t end;
+  int next;
+  int pre;
+}Node;
+
+Node list[NUM];
+int pool[NUM] = {0};
+static int top = 0;
+
+int get_start(uintptr_t start, uintptr_t end, size_t size, uintptr_t t) {
+  uintptr_t s = 0;
+  while (s < start) {
+    s += t;
+  }
+  if (end - s > size) {
+    return s;
+  }
+  return -1;
+}
+
+int get_free_id() {
+  assert(top > 0);
+  return pool[--top];
+}
+
+void put_id(int v) {
+  assert(top <= NUM);
+  pool[top++] = v;
+}
 
 static void *kalloc(size_t size) {
-  return NULL;
+  assert(size < 16 * 1024 * 1024);
+  while (atomic_xchg(&lock, 1) == 1) {
+
+  }
+  uintptr_t s = 0;
+  for (int i = 0; i != -1; ) {
+    
+    int next = list[i].next;
+    assert(next != -1);
+    uintptr_t t = 1;
+    while (t < size) {
+      t << 1;
+    }
+    if (s = get_start(list[i].end, list[next].start, size, t) != -1) {
+      int id = get_free_id();
+      list[id].next = next;
+      list[id].pre = i;
+      list[id].start = s;
+      list[id].end = s + size;
+      list[i].next = id;
+      list[next].pre = id;
+      break;
+    }
+    i = next;
+  }
+  atomic_xchg(&lock, 0);
+  return s;
 }
 
 static void kfree(void *ptr) {
+  int i = 0;
+
+  while (atomic_xchg(&lock, 1) == 1) {
+
+  }
+  while (list[i].next != -1) {
+    if (list[i].start == ptr) {
+      put_id(i);
+      int pre = list[i].pre;
+      int next = list[i].next;
+      list[pre].next = next;
+      list[next].pre = pre;
+    }
+  }
+  atomic_xchg(&lock, 0);
 }
 
 #ifdef TEST
@@ -24,6 +99,17 @@ static void pmm_init() {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  list[0].next = 1;
+  list[0].pre = -1
+  list[0].start = heap.start;
+  list[0].end = heap.end;
+  list[1].next = -1;
+  list[1].pre = 0;
+  list[1].start = heap.end;
+  list[1].end = heap.end;
+  for (int i = 2; i < NUM; i++) {
+    pool[top++] = i;
+  }
 }
 #endif
 MODULE_DEF(pmm) = {
