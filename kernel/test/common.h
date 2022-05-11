eccd165166307f624e55bbe557cdd766e3c93a59
tracer-nju <tracer@nju.edu.cn>
2022-05-11 15:43:12 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:12 up  2:34,  1 user,  load average: 0.48, 0.34, 0.24

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <kernel.h>
-#include <klib.h>
-#include <klib-macros.h>
+// #include <klib.h>
+// #include <klib-macros.h>
 
 #define HEAP_SIZE 134217728
 
@@ -18,18 +18,18 @@ typedef struct {
 
 Area heap;
 
-#define MODULE(mod) \
-  typedef struct mod_##mod##_t mod_##mod##_t; \
-  extern mod_##mod##_t *mod; \
-  struct mod_##mod##_t
+// #define MODULE(mod) \
+//   typedef struct mod_##mod##_t mod_##mod##_t; \
+//   extern mod_##mod##_t *mod; \
+//   struct mod_##mod##_t
 
-#define MODULE_DEF(mod) \
-  extern mod_##mod##_t __##mod##_obj; \
-  mod_##mod##_t *mod = &__##mod##_obj; \
-  mod_##mod##_t __##mod##_obj
-MODULE(pmm) {
-  void  (*init)();
-  void *(*alloc)(size_t size);
-  void  (*free)(void *ptr);
-};
+// #define MODULE_DEF(mod) \
+//   extern mod_##mod##_t __##mod##_obj; \
+//   mod_##mod##_t *mod = &__##mod##_obj; \
+//   mod_##mod##_t __##mod##_obj
+// MODULE(pmm) {
+//   void  (*init)();
+//   void *(*alloc)(size_t size);
+//   void  (*free)(void *ptr);
+// };
 
