ed12bb7b6596679fe8aa8cdd936bcb92798744db
tracer-nju <tracer@nju.edu.cn>
2022-05-11 17:03:52 +0800

    Linux s-B450-AORUS-PRO-WIFI 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:52 up  3:55,  1 user,  load average: 0.35, 0.22, 0.20

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -59,7 +59,7 @@ static void *kalloc(size_t size) {
     }
     s = get_start(list[i].end, list[next].start, size, t);
     if (s != -1) {
-      printf("%lu  %d %d\n", s, t, s%t);
+      // printf("%lu  %d %d\n", s, t, s%t);
       assert (s % t == 0);
       int id = get_free_id();
       list[id].next = next;
